376172a5fa85253061f937c2f31ffaac592df6c3
Hasan Hüseyin Pay <hhuseyinpay@gmail.com>
2017-11-09 10:02:16 +0300

    fifo test

---
+++ systemProgramming/fifoclient.c
@@ -0,0 +1,25 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define FIFO_FILE       "MYFIFO"
+
+int main(int argc, char *argv[])
+{
+        FILE *fp;
+
+        if ( argc != 2 ) {
+                printf("USAGE: fifoclient [string]\n");
+                exit(1);
+        }
+
+        if((fp = fopen(FIFO_FILE, "w")) == NULL) {
+                perror("fopen");
+                exit(1);
+        }
+
+        fputs(argv[1], fp);
+
+        fclose(fp);
+        return(0);
+}
