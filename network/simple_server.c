636fffec54de545b8fa1031485ecad805888254a
Hasan Hüseyin Pay <h.huseyinpay@gmail.com>
2017-11-29 19:44:25 +0300

    a

--- network/simple_server.c
+++ network/simple_server.c
@@ -27,6 +27,7 @@ int main(int argc, char *argv[])
     serv_addr.sin_addr.s_addr = INADDR_ANY;
     serv_addr.sin_port = htons(portno);
 
+
     if( bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0 ) {
         perror("ERROR on binding");
         exit(1);
